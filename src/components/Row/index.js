cba62d21f996a4c9dc71819ee158a88a68636ccd
Tynan DeBold <tynan.debold@tradeshift.com>
2018-10-25 20:29:38 +0200

    linking to the show's detail page in table

--- src/components/Row/index.js
+++ src/components/Row/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import moment from 'moment';
 
-export default ({ datetime, venue }) => (
+export default ({ datetime, url, venue }) => (
   <tr key={datetime}>
-    <td>{venue.name}</td>
+    <td><a href={url} target="_blank">{venue.name}</a></td>
     <td>{venue.city}</td>
     <td>{venue.country}</td>
     <td>{moment(datetime).format("DD MMM YYYY")}</td>
